020aeb5b5704a01126d36fe95036a9cf2fcd85ae
DJT777 <djt.2012.777@gmail.com>
2022-11-01 19:27:04 -0500

    cleaned up main from having function definitions

--- main.py
+++ main.py
@@ -19,59 +19,6 @@ app = Flask(__name__)
 class_json = open('data.json')
 class_data = json.load(class_json)
 
-def create_embeddings(userInput):
-    module = hub.Module("https://tfhub.dev/google/universal-sentence-encoder-lite/2")
-    # module = hub.Module("module\universal-sentence-encoder-lite_2\tfhub_module.pb")
-    input_placeholder = tf.sparse_placeholder(tf.int64, shape=[None, None])
-    encodings = module(
-        inputs=dict(
-            values=input_placeholder.values,
-            indices=input_placeholder.indices,
-            dense_shape=input_placeholder.dense_shape))
-
-    with tf.Session() as sess:
-        spm_path = sess.run(module(signature="spm_path"))
-
-    sp = spm.SentencePieceProcessor()
-    with tf.io.gfile.GFile(spm_path, mode="rb") as f:
-        sp.LoadFromSerializedProto(f.read())
-    #print("SentencePiece model loaded at {}.".format(spm_path))
-
-    messages = []
-    messages.append(userInput)
-
-    values, indices, dense_shape = process_to_IDs_in_sparse_format(sp, messages)
-
-    # Reduce logging output.
-    logging.set_verbosity(logging.ERROR)
-
-    with tf.Session() as session:
-        session.run([tf.global_variables_initializer(), tf.tables_initializer()])
-        message_embeddings = session.run(
-            encodings,
-            feed_dict={input_placeholder.values: values,
-                       input_placeholder.indices: indices,
-                       input_placeholder.dense_shape: dense_shape})
-
-        #print(message_embeddings[0])
-        return message_embeddings[0]
-        #for i, message_embedding in enumerate(np.array(message_embeddings).tolist()):
-        #    print("Message: {}".format(messages[i]))
-        #   print("Embedding size: {}".format(len(message_embedding)))
-        #    message_embedding_snippet = ", ".join(
-        #        (str(x) for x in message_embedding[:3]))
-        #    print("Embedding: [{}, ...]\n".format(message_embedding_snippet))
-
-def process_to_IDs_in_sparse_format(sp, sentences):
-  # An utility method that processes sentences with the sentence piece processor
-  # 'sp' and returns the results in tf.SparseTensor-similar format:
-  # (values, indices, dense_shape)
-  ids = [sp.EncodeAsIds(x) for x in sentences]
-  max_len = max(len(x) for x in ids)
-  dense_shape=(len(ids), max_len)
-  values=[item for sublist in ids for item in sublist]
-  indices=[[row,col] for row in range(len(ids)) for col in range(len(ids[row]))]
-  return (values, indices, dense_shape)
 
 def filter_course(course):
     if(course not in class_data):
@@ -95,8 +42,6 @@ def findcourses():
                 print("User Description: " + user_description)
                 user_description_embedding = p1.create_embeddings(user_description)
                 recommendation = p1.query_embedding(user_description_embedding, user_description)
-                # print(labels_to_return)
-                # print(recommendations_user_text)
                 return render_template('findcourses.html',dropdownList=dropdown_list,
                                         recommendedClasses=recommendation.recommendations_user_text, containsData="True", containsDataMajor="False")
             if request.form['submitButton'] == "Find Similar":
@@ -131,7 +76,7 @@ def index():
             output = item
         # print(output.items())
         # return '<h1>Hello Default</h1>
-        quickEmbbedings = create_embeddings(output)
+        quickEmbbedings = p1.create_embeddings(output)
         p = hnswlib.Index(space='cosine', dim=512)
         p.load_index("./notebooks/index.bin")
         labels, distances = p.knn_query(quickEmbbedings, k=5)
