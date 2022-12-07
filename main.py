77150616b378f6c1bf193a8d6a887b1762e181ee
DJT777 <djt.2012.777@gmail.com>
2022-12-07 04:18:39 -0600

    working on colab, exceeds local machine resources

--- main.py
+++ main.py
@@ -1,38 +1,22 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 from absl import logging
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+import tensorflow as tf
 import tensorflow_hub as hub
-import sentencepiece as spm
-import matplotlib.pyplot as plt
-import numpy as np
 import os
-import pandas as pd
 import re
-import seaborn as sns
 import hnswlib
 import json
-import sentencepiece
-import laUSE as aq
-from  database import  database
+import laUSEv5 as aq
+from database import database
 from visitor import Visitor
 from form import Form
+#from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
+#run_with_ngrok(app)
 class_json = open('data.json')
 class_data = json.load(class_json)
-major_dict = {"ACCT": "Accounting", "ADV":"Advertising", "AFRC":"Africana Studies", "AGBU":"Agricultural Business", "AHA":"Arts and Heritage Administration", "ANTH":"Anthropology", "ARAB":"Arabic Studies", "ART":"Art",
-              "ARTD":"Art & Design", "ARTE":"Art Education", "ARTH":"Art History", "ARTP":"Photography", "ASTR":"Astronomy", "BAN":"Business Analytics", "BCOM":"Business Communication", "BIOL":"Biology",
-              "BLAW":"Business Law", "BUAD":"Business Administration", "CE":"Civil Engineering", "CHEM":"Chemistry", "CHIN":"Chinese", "CIS":"Computer Information Systems", "CMST":"Communication Studies", "COMM":"Communications",
-              "CRIM": "Criminology", "CS":"Computer Science", "DMS":"Diagnostic Medical Sonography", "DSCI":"Data Science", "DTAS":"Dental Assisting", "DTHY":"Dental Hygiene", "DVT":"Diagnostic Vascular Sonography", "ECE":"Electrical Engineering",
-              "ECHO":"Echocardiography", "ECON": "Economics", "EDUC": "Education", "ENG":"English", "ENGR":"Engineering", "EXSC":"Exercise Science", "FIN":"Finance", "FREN":"French", "GENS":"Generla Studies", "GEOG":"Geography",
-              "GEOL":"Geology", "GERM":"German", "GERO":"Gerontology", "GLST":"Global Studies", "GNDR":"Gender Studies", "HA":"Health Administration", "HI":"Health Informatics and Information Management", "HIST":"History",
-              "HONS":"Honors", "HP":"Health Professions", "HUM":"Humanities", "IEP":"Intensive English Program", "IME":"Industrial and Manufacturing Engineering", "IPH":"Interprofessional Health", "JPN":"Japanese",
-              "JRN":"Journalism", "KIN":"Kinesiology", "LATN":"Latin", "LIBA":"Liberal Arts", "MATH":"Mathematics", "ME":"Mechanical Engineering", "MFET":"Manufacting Engineering Technology", "MKTG":"Marketing", "MNGT":"Management",
-              "MS":"Military Science", "MUS":"Music", "NURS":"Nursing", "NUTR":"Nutrition", "OTA":"Occupational Therapy Assistant", "PET":"Physical Education Teaching", "PH":"Public Health", "PHIL":"Philosophy", "PHYS":"Physics", "POLS":"Political Science",
-              "PRFS":"Professional Studies", "PRL":"Public Relations", "PSY":"Psychology", "RADT":"Radiologic Technology", "RELS":"Religious Studies", "REST":"Respiratory Therapy", "RTV":"Radio and Television", "SOC":"Sociology",
-              "SOCW":"Social Work", "SPAN":"Spanish", "SPTM":"Sports Management", "STAT":"Statistics", "STEM":"Science, Technology, Engineering, & Mathematics", "TECH":"Technology", "THTR":"Theatre", "UNIV":"University Studies",
-              "WLC":"World Languages and Cultures"}
+
 
 
 @app.route("/findcourses", methods=['GET', 'POST', "PUT"])
@@ -53,7 +37,7 @@ def findcourses():
                 myDb.insertClass(recommendation.recommendations_user_text, 'describeClass')
                 myDb.insertClass(recommendation.recommendations_user_text, 'classlist')
                 return render_template('findcourses.html',dropdownList=dropdown_list,
-                                        recommendedClasses=recommendation.recommendations_user_text, containsData="True", 
+                                        recommendedClasses=recommendation.recommendations_user_text, containsData="True",
                                         containsDataMajor="False", userQuery = user_description)
             if request.form['submitButton'] == "Find Similar":
                 print("SELECTED CLASS:" + request.form['selectClass'])
@@ -65,18 +49,15 @@ def findcourses():
                 for recommended_class in recommendation.recommendations_user_text:
                     myDb.courseExists(recommended_class, 'selectClass')
                 return render_template("findcourses.html", dropdownList=dropdown_list,
-                                        recommendedClasses = recommendation.recommendations_user_text, containsData="True", 
+                                        recommendedClasses = recommendation.recommendations_user_text, containsData="True",
                                         containsDataMajor="False",userQuery = selectedClass)
             if request.form['submitButton'] == "Find a Major":
                 print("MAJOR INTERESTS:" + request.form['userInputMajor'])
                 majorDescription = request.form['userInputMajor']
-                selectedMajorEmbedding = p1.create_embeddings(majorDescription)
-                recommendation = p1.query_embedding(selectedMajorEmbedding, majorDescription)
-                return_recommendation = []
-
-                return_recommendation.append(recommendation.recommended_major + " - " + major_dict[recommendation.recommended_major])
+                code, classification = p1.classify(majorDescription)
+                return_recommendation = [code + " - " + classification]
                 # print("Recommendation: ",return_recommendation);
-                myDb.insertMajor('describeMajor', major_dict[recommendation.recommended_major])
+                myDb.insertMajor('describeMajor', classification)
                 return render_template("findcourses.html", dropdownList=dropdown_list,
                                         recommendedClasses =return_recommendation, containsData="False", containsDataMajor="True",
                                         userQuery = majorDescription)
@@ -148,4 +129,4 @@ if __name__ == '__main__':
     visitor = Visitor(myDb)
     visitor.logVisitor()
     form = Form(myDb)
-    app.run(debug=True)
+    app.run()
